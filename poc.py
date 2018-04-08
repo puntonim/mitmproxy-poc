de576223a99bb95176904d2a682b766ea837b405
Paolo C <6423485+puntonim@users.noreply.github.com>
2018-04-08 23:22:55 +0200

    NEW Add readme

--- poc.py
+++ poc.py
@@ -1,19 +1,3 @@
-"""
-MITMPROXY
-https://mitmproxy.org/
-
-How to run this examples:
-- create a python3 virtual environment
-- install the requirements in requirements.txt
-- run:
-$ mitmdump -s poc.py
-- now make a request using the proxy like:
-$ curl -i -x 127.0.0.1:8080 google.com
-
-More examples here:
-https://github.com/mitmproxy/mitmproxy/tree/master/examples
-"""
-
 from bs4 import BeautifulSoup
 
 from mitmproxy import http
